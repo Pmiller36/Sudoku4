2a02393a0139ca4c513e31acbaf56eafcad22318
Pmiller36 <pmille36@students.kennesaw.edu>
2016-01-26 14:28:38 -0500

    First Commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -8,9 +8,22 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        //Globals
+        Slot temp = new Slot(0,0);
+        public void CreateBlankPuzzle()
+        {
+            for (int i = 0; i < 81; i++)
+            {
+                temp = new Slot((i + 1) % 9, i);
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            int firstVariable = 0;
+            //CreateBlankPuzzle();
+
+
 
             Console.ReadLine();
         }
---
+++ ConsoleApplication1/Slot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Slot
+    {
+        public int num;
+        public int pos;
+        Slot temp = new Slot(0, 0);
+
+        public Slot(int number, int position)
+        {
+            if (number > 9)
+            {
+                number = 0;
+            }
+            else if (number < 0)
+            {
+                number = 0;
+            }
+
+            num = number;
+            pos = position;
+        }
+
+        //public void createblankpuzzle()
+        //{
+        //    for (int i = 0; i < 81; i++)
+        //    {
+        //        temp = new slot((i + 1) % 9, i);
+        //    }
+        //}
+
+
+    }
+}
