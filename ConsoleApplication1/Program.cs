98f62a13026d91676e5414549d3f190015d83ef0
Patrick Miller <pmille36@students.kennesaw.edu>
2016-01-30 21:29:35 -0500

    Finished all requirements!

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -16,18 +16,54 @@ namespace ConsoleApplication1
         {
             Slot tempMain = new Slot(0, 0);
             Slot tempAdd = new Slot(0, 0);
-            string directoryForPuzzle = "%%/ConsoleApplication1/ConsoleApplication1/bin/Debug/Sudoku Test.txt";
+            string directoryForPuzzle = "%%/ConsoleApplication1/ConsoleApplication1/bin/Debug/Sudoku Test 5.txt";
             int totalPrinted = 0;
             List<Slot> sampleSlotList = new List<Slot>();
             List<Slot> actualPuzzle = new List<Slot>();
             List<Int16> actualPuzzleNumbers = new List<Int16>();
             string actualNumbersRead = "";
+            List<Slot> puzzleSolution = new List<Slot>();
+            int errors = 0;
+
+            //puzzle string sets
+            string set1, set2, set3, set4, set5, set6, set7, set8, set9;
+            set1 = set2 = set3 = set4 = set5 = set6 = set7 = set8 = set9 = "";
+
+            //solution string sets
+            string set1s, set2s, set3s, set4s, set5s, set6s, set7s, set8s, set9s;
+            set1s = set2s = set3s = set4s = set5s = set6s = set7s = set8s = set9s = "";
+            
+            
 
             try
             {
-                using (StreamReader sr = new StreamReader("Sudoku Test.txt"))
+                //using (StreamReader sr = new StreamReader("Sudoku Test 2.txt"))
+                //{
+                //    actualNumbersRead = sr.ReadToEnd();
+                //}
+                using (StreamReader sr = new StreamReader("Sudoku Test 5.txt"))
                 {
-                    actualNumbersRead = sr.ReadToEnd();
+                    set1 = sr.ReadLine();
+                    set2 = sr.ReadLine();
+                    set3 = sr.ReadLine();
+                    set4 = sr.ReadLine();
+                    set5 = sr.ReadLine();
+                    set6 = sr.ReadLine();
+                    set7 = sr.ReadLine();
+                    set8 = sr.ReadLine();
+                    set9 = sr.ReadLine();
+                }
+                using (StreamReader sr = new StreamReader("Sudoku Test 5 solution.txt"))
+                {
+                    set1s = sr.ReadLine();
+                    set2s = sr.ReadLine();
+                    set3s = sr.ReadLine();
+                    set4s = sr.ReadLine();
+                    set5s = sr.ReadLine();
+                    set6s = sr.ReadLine();
+                    set7s = sr.ReadLine();
+                    set8s = sr.ReadLine();
+                    set9s = sr.ReadLine();
                 }
             }
             catch (Exception e)
@@ -35,14 +71,213 @@ namespace ConsoleApplication1
                 Console.WriteLine("The file could not be read:");
                 Console.WriteLine(e.Message);
             }
+
             Char delimiter = ',';
             String[] substrings = actualNumbersRead.Split(delimiter);
+            String[] sub1 = set1.Split(delimiter);
+            String[] sub2 = set2.Split(delimiter);
+            String[] sub3 = set3.Split(delimiter);
+            String[] sub4 = set4.Split(delimiter);
+            String[] sub5 = set5.Split(delimiter);
+            String[] sub6 = set6.Split(delimiter);
+            String[] sub7 = set7.Split(delimiter);
+            String[] sub8 = set8.Split(delimiter);
+            String[] sub9 = set9.Split(delimiter);
 
-            for(int i = 0; i < 81; i++)
+            String[] sub1s = set1s.Split(delimiter);
+            String[] sub2s = set2s.Split(delimiter);
+            String[] sub3s = set3s.Split(delimiter);
+            String[] sub4s = set4s.Split(delimiter);
+            String[] sub5s = set5s.Split(delimiter);
+            String[] sub6s = set6s.Split(delimiter);
+            String[] sub7s = set7s.Split(delimiter);
+            String[] sub8s = set8s.Split(delimiter);
+            String[] sub9s = set9s.Split(delimiter);
+
+
+
+            for(int i = 0; i < 9; i++)
             {
-                tempAdd = new Slot(Int32.Parse(substrings[i]), i);
-                actualPuzzle.Add(tempAdd);
+                if (sub1[i] == "")
+                {
+                    tempAdd = new Slot(0, i);
+                    actualPuzzle.Add(tempAdd);
+                }
+                else
+                {
+                    tempAdd = new Slot(Int32.Parse(sub1[i]), i);
+                    actualPuzzle.Add(tempAdd);
+                }
             }
+            for (int i = 0; i < 9; i++)
+            {
+                if (sub2[i] == "")
+                {
+                    tempAdd = new Slot(0, i+9);
+                    actualPuzzle.Add(tempAdd);
+                }
+                else
+                {
+                    tempAdd = new Slot(Int32.Parse(sub2[i]), i+9);
+                    actualPuzzle.Add(tempAdd);
+                }
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (sub3[i] == "")
+                {
+                    tempAdd = new Slot(0, i+18);
+                    actualPuzzle.Add(tempAdd);
+                }
+                else
+                {
+                    tempAdd = new Slot(Int32.Parse(sub3[i]), i+18);
+                    actualPuzzle.Add(tempAdd);
+                }
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (sub4[i] == "")
+                {
+                    tempAdd = new Slot(0, i+27);
+                    actualPuzzle.Add(tempAdd);
+                }
+                else
+                {
+                    tempAdd = new Slot(Int32.Parse(sub4[i]), i+27);
+                    actualPuzzle.Add(tempAdd);
+                }
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (sub5[i] == "")
+                {
+                    tempAdd = new Slot(0, i+36);
+                    actualPuzzle.Add(tempAdd);
+                }
+                else
+                {
+                    tempAdd = new Slot(Int32.Parse(sub5[i]), i+36);
+                    actualPuzzle.Add(tempAdd);
+                }
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (sub6[i] == "")
+                {
+                    tempAdd = new Slot(0, i+45);
+                    actualPuzzle.Add(tempAdd);
+                }
+                else
+                {
+                    tempAdd = new Slot(Int32.Parse(sub6[i]), i+45);
+                    actualPuzzle.Add(tempAdd);
+                }
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (sub7[i] == "")
+                {
+                    tempAdd = new Slot(0, i+54);
+                    actualPuzzle.Add(tempAdd);
+                }
+                else
+                {
+                    tempAdd = new Slot(Int32.Parse(sub7[i]), i+54);
+                    actualPuzzle.Add(tempAdd);
+                }
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (sub8[i] == "")
+                {
+                    tempAdd = new Slot(0, i+63);
+                    actualPuzzle.Add(tempAdd);
+                }
+                else
+                {
+                    tempAdd = new Slot(Int32.Parse(sub8[i]), i+63);
+                    actualPuzzle.Add(tempAdd);
+                }
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (sub9[i] == "")
+                {
+                    tempAdd = new Slot(0, i+72);
+                    actualPuzzle.Add(tempAdd);
+                }
+                else
+                {
+                    tempAdd = new Slot(Int32.Parse(sub9[i]), i+72);
+                    actualPuzzle.Add(tempAdd);
+                }
+            }
+
+
+
+            //Input the solution numbers
+            for (int i = 0; i < 9; i++)
+            {
+                tempAdd = new Slot(Int32.Parse(sub1s[i]), i);
+                puzzleSolution.Add(tempAdd);
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                tempAdd = new Slot(Int32.Parse(sub2s[i]), i+9);
+                puzzleSolution.Add(tempAdd);
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                tempAdd = new Slot(Int32.Parse(sub3s[i]), i+18);
+                puzzleSolution.Add(tempAdd);
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                tempAdd = new Slot(Int32.Parse(sub4s[i]), i+27);
+                puzzleSolution.Add(tempAdd);
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                tempAdd = new Slot(Int32.Parse(sub5s[i]), i+36);
+                puzzleSolution.Add(tempAdd);
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                tempAdd = new Slot(Int32.Parse(sub6s[i]), i+45);
+                puzzleSolution.Add(tempAdd);
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                tempAdd = new Slot(Int32.Parse(sub7s[i]), i+54);
+                puzzleSolution.Add(tempAdd);
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                tempAdd = new Slot(Int32.Parse(sub8s[i]), i+63);
+                puzzleSolution.Add(tempAdd);
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                tempAdd = new Slot(Int32.Parse(sub9s[i]), i+72);
+                puzzleSolution.Add(tempAdd);
+            }
+
+
+
+
+            //for (int i = 0; i < 81; i++)
+            //{
+            //    if (substrings[i] == "")
+            //    {
+            //        substrings[i] = "0";
+            //    }
+            //    tempAdd = new Slot(Int32.Parse(substrings[i]), i);
+            //    actualPuzzle.Add(tempAdd);
+            //}
+
+
+
             tempMain.CreateBlankPuzzle(sampleSlotList);
 
             Console.WriteLine("Welcome to Sudoku Solver!!! Below is a Sample puzzle. " + "\n" + "Please enter your puzzle at: \n" + "\"" + directoryForPuzzle + "\"");
@@ -75,6 +310,32 @@ namespace ConsoleApplication1
                 }
             }
 
+            Console.ReadLine();
+
+            //static amount of solve times
+            //for (int i = 0; i < 500; i++)
+            //{
+            //    tempMain.SolveRowsAndColumns(actualPuzzle);
+            //}
+
+            //dynamic amount of solve times
+
+            tempMain.SolveAndPrint(actualPuzzle);
+
+            Console.WriteLine("Checking with solution...");
+            for (int i = 0; i < 81; i++)
+            {
+                if (actualPuzzle[i].num != puzzleSolution[i].num)
+                {
+                    errors++;
+                }
+            }
+            Console.WriteLine("There were " + errors + " errors, as checked with the solution.");
+
+
+
+            Console.WriteLine("\n \n \n \n \n");
+            Console.WriteLine("The program has ended. Press enter to exit.");
             Console.ReadLine();
         }
     }
--- ConsoleApplication1/Slot.cs
+++ ConsoleApplication1/Slot.cs
@@ -11,6 +11,12 @@ namespace ConsoleApplication1
         public int num;
         public int pos;
         Slot temp;
+        public int row, column, nonet;
+        public int currentRow, currentColumn, currentNonet;
+        bool detects1, detects2, detects3, detects4, detects5, detects6, detects7, detects8, detects9;
+        int totalPrinted;
+        int totalMissing;
+
 
 
         public Slot(int number, int position)
@@ -26,6 +32,58 @@ namespace ConsoleApplication1
 
             num = number;
             pos = position;
+            //mod determines the column, division determines row. 
+            column = pos % 9;
+            row = pos / 9;
+
+            //hella loops determine nonets:
+            #region Determine Nonets
+            if (row < 3)
+            {
+                if (column < 3)
+                {
+                    nonet = 0;
+                }
+                else if (column >= 3 && column <= 5)
+                {
+                    nonet = 1;
+                }
+                else if (column > 5)
+                {
+                    nonet = 2;
+                }
+            }
+            else if (row >= 3 && row <= 5)
+            {
+                if (column < 3)
+                {
+                    nonet = 3;
+                }
+                else if (column >= 3 && column <= 5)
+                {
+                    nonet = 4;
+                }
+                else if (column > 5)
+                {
+                    nonet = 5;
+                }
+            }
+            else if (row > 5)
+            {
+                if (column < 3)
+                {
+                    nonet = 6;
+                }
+                else if (column >= 3 && column <= 5)
+                {
+                    nonet = 7;
+                }
+                else if (column > 5)
+                {
+                    nonet = 8;
+                }
+            }
+            #endregion
         }
 
         public void CreateBlankPuzzle(List<Slot> slotList)
@@ -36,13 +94,231 @@ namespace ConsoleApplication1
                 slotList.Add(temp);
             }
         }
-        public List<Slot> solveRowsAndColumns(List<Slot> listBeforeSolve)
+        public bool PuzzleSolved(List<Slot> slotList)
+        {
+            totalMissing = 0;
+            foreach (Slot s in slotList)
+            {
+                if (s.num == 0)
+                {
+                    totalMissing++;
+                }
+            }
+            if (totalMissing > 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public int NumbersMissing(List<Slot> slotList)
+        {
+            totalMissing = 0;
+            foreach (Slot s in slotList)
+            {
+                if (s.num == 0)
+                {
+                    totalMissing++;
+                }
+            }
+            return totalMissing;
+        }
+        public void PrintPuzzle(List<Slot> slotList)
+        {
+            totalPrinted = 0;
+            foreach (Slot s in slotList)
+            {
+                if (s.pos % 27 == 0)
+                {
+                    Console.WriteLine("_______________________");
+                }
+
+                Console.Write(s.num + " ");
+
+                if ((s.pos + 1) % 3 == 0)
+                {
+                    Console.Write("| ");
+                }
+
+                totalPrinted++;
+
+                if (totalPrinted % 9 == 0)
+                {
+                    //totalPrinted = 0;
+                    Console.WriteLine();
+                }
+
+                if (totalPrinted == 81)
+                {
+                    Console.WriteLine("_______________________");
+                }
+            }
+        }
+        public List<Slot> SolveRowsAndColumns(List<Slot> listBeforeSolve)
         {
             List<Slot> newList = new List<Slot>();
-            newList.Add(new Slot(0, 0));
+            newList = listBeforeSolve;
+
+
+            #region Actual Solving here
+            foreach (Slot s in newList)
+            {
+                if (s.num == 0) //first, make sure the current slot isn't already filled. If it is, do nothing.
+                {
+                    //at the start, set everything back to false. 
+                    detects1 = detects2 = detects3 = detects4 = detects5 = detects6 = detects7 = detects8 = detects9 = false;
+                    //first, identify row, column, and nonet of 's'.
+                    //The first 's' has a position of 0: it is in the 0 row and 0 column and 0 nonet.
+                    currentColumn = s.column;
+                    currentRow = s.row;
+                    currentNonet = s.nonet;
+
+                    //Next, Check each position's row, column, and nonet for numbers. Toggle booleans to keep track.
+                    #region rowCheck
+                    foreach (Slot rowCheck in newList)
+                    {
+                        if (rowCheck.row == currentRow) //they are in the same row
+                        {
+                            if (rowCheck.num == 1)
+                                detects1 = true;
+                            else if (rowCheck.num == 2)
+                                detects2 = true;
+                            else if (rowCheck.num == 3)
+                                detects3 = true;
+                            else if (rowCheck.num == 4)
+                                detects4 = true;
+                            else if (rowCheck.num == 5)
+                                detects5 = true;
+                            else if (rowCheck.num == 6)
+                                detects6 = true;
+                            else if (rowCheck.num == 7)
+                                detects7 = true;
+                            else if (rowCheck.num == 8)
+                                detects8 = true;
+                            else if (rowCheck.num == 9)
+                                detects9 = true;
+                        }
+                    }
+                    #endregion
+
+                    #region columnCheck
+                    foreach (Slot columnCheck in newList)
+                    {
+                        if (columnCheck.column == currentColumn) //they are in the same column
+                        {
+                            if (columnCheck.num == 1)
+                                detects1 = true;
+                            else if (columnCheck.num == 2)
+                                detects2 = true;
+                            else if (columnCheck.num == 3)
+                                detects3 = true;
+                            else if (columnCheck.num == 4)
+                                detects4 = true;
+                            else if (columnCheck.num == 5)
+                                detects5 = true;
+                            else if (columnCheck.num == 6)
+                                detects6 = true;
+                            else if (columnCheck.num == 7)
+                                detects7 = true;
+                            else if (columnCheck.num == 8)
+                                detects8 = true;
+                            else if (columnCheck.num == 9)
+                                detects9 = true;
+                        }
+                    }
+                    #endregion
+
+                    #region nonetCheck
+                    foreach (Slot nonetCheck in newList)
+                    {
+                        if (nonetCheck.nonet == currentNonet) //they are in the same nonet
+                        {
+                            if (nonetCheck.num == 1)
+                                detects1 = true;
+                            else if (nonetCheck.num == 2)
+                                detects2 = true;
+                            else if (nonetCheck.num == 3)
+                                detects3 = true;
+                            else if (nonetCheck.num == 4)
+                                detects4 = true;
+                            else if (nonetCheck.num == 5)
+                                detects5 = true;
+                            else if (nonetCheck.num == 6)
+                                detects6 = true;
+                            else if (nonetCheck.num == 7)
+                                detects7 = true;
+                            else if (nonetCheck.num == 8)
+                                detects8 = true;
+                            else if (nonetCheck.num == 9)
+                                detects9 = true;
+                        }
+                    }
+                    #endregion
+
+                    //Place numbers.
+                    //If all but 1 number is detected, input that remaining number
+                    if (!detects1 && detects2 && detects3 && detects4 && detects5 && detects6 && detects7 && detects8 && detects9)
+                        s.num = 1;   //detects everything but 1
+                    else if (detects1 && !detects2 && detects3 && detects4 && detects5 && detects6 && detects7 && detects8 && detects9)
+                        s.num = 2;   //detects everything but 2
+                    else if (detects1 && detects2 && !detects3 && detects4 && detects5 && detects6 && detects7 && detects8 && detects9)
+                        s.num = 3;   //detects everything but 3
+                    else if (detects1 && detects2 && detects3 && !detects4 && detects5 && detects6 && detects7 && detects8 && detects9)
+                        s.num = 4;   //detects everything but 4
+                    else if (detects1 && detects2 && detects3 && detects4 && !detects5 && detects6 && detects7 && detects8 && detects9)
+                        s.num = 5;   //detects everything but 5
+                    else if (detects1 && detects2 && detects3 && detects4 && detects5 && !detects6 && detects7 && detects8 && detects9)
+                        s.num = 6;   //detects everything but 6
+                    else if (detects1 && detects2 && detects3 && detects4 && detects5 && detects6 && !detects7 && detects8 && detects9)
+                        s.num = 7;   //detects everything but 7
+                    else if (detects1 && detects2 && detects3 && detects4 && detects5 && detects6 && detects7 && !detects8 && detects9)
+                        s.num = 8;   //detects everything but 8
+                    else if (detects1 && detects2 && detects3 && detects4 && detects5 && detects6 && detects7 && detects8 && !detects9)
+                        s.num = 9;   //detects everything but 9
+                }
+            }
+
+            #endregion
 
             return newList;
         }
+        public void SolveAndPrint(List<Slot> slotList)
+        {
+            int iterations = 0;
+            Console.WriteLine("The puzzle is missing " + NumbersMissing(slotList) + " numbers.");
+            Console.WriteLine("Press enter to see the next iteration.");
+            Console.ReadLine();
 
+            while (NumbersMissing(slotList) > 0)
+            {
+                SolveRowsAndColumns(slotList);
+                PrintPuzzle(slotList);
+
+                if (NumbersMissing(slotList) > 0)
+                {
+                    Console.Write("The puzzle is now only missing " + NumbersMissing(slotList));
+                    if (NumbersMissing(slotList) == 1)
+                    {
+                        Console.WriteLine(" number.");
+                    }
+                    else if (NumbersMissing(slotList) > 1)
+                    {
+                        Console.WriteLine(" numbers.");
+                    }
+                    Console.WriteLine("Press enter to see the next iteration.");
+                    Console.WriteLine("_____________________________________________________________________________");
+                }
+                else if (NumbersMissing(slotList) == 0)
+                {
+                    Console.WriteLine("Done! Press enter for more information.");
+                }
+
+                Console.ReadLine();
+                iterations++;
+            }
+            Console.WriteLine("This took " + iterations + " passes through to solve!");
+        }
     }
 }
